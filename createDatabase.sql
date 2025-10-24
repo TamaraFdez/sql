5171b92c07776540bd07d4b5a9339aca99f5725a
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-10-24 16:18:25 +0200

    add foreign key

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,36 +1,40 @@
 CREATE DATABASE IF NOT EXISTS borjamoll;
 USE borjamoll;
+
 DROP TABLE IF EXISTS Student;
 DROP TABLE IF EXISTS Employee;
 DROP TABLE IF EXISTS Company;
 DROP TABLE IF EXISTS Teacher;
 
 CREATE TABLE Teacher (
-    id INT primary key auto_increment,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     teacherName VARCHAR(250),
     email VARCHAR(250)
 );
 
 CREATE TABLE Company (
-    id INT primary key auto_increment,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     companyName VARCHAR(250),
     email VARCHAR(250)
 );
 
-CREATE TABLE Employee(
-    id INT primary key auto_increment,
+CREATE TABLE Employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     employeeName VARCHAR(250),
     email VARCHAR(250),
-    company_id INT REFERENCES Company(id)
+    company_id INT,
+    FOREIGN KEY (company_id) REFERENCES Company(id)
 );
 
-CREATE TABLE Student(
-    id INT primary key auto_increment,
+CREATE TABLE Student (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     studentName VARCHAR(250),
     surname VARCHAR(250),
     email VARCHAR(250),
-    teacher_id INT REFERENCES Teacher(id),
-    employee_id INT REFERENCES Employee(id),
-    phone_number VARCHAR(15)
+    teacher_id INT,
+    employee_id INT,
+    phone_number VARCHAR(15),
+    FOREIGN KEY (teacher_id) REFERENCES Teacher(id),
+    FOREIGN KEY (employee_id) REFERENCES Employee(id)
 );
 
