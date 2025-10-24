32c59cd4a3b012b4cf5d61f6f83069fca6ae8151
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-10-24 17:24:29 +0200

    firsts queries

---
+++ queries.sql
@@ -0,0 +1,15 @@
+USE borjamoll;
+
+INSERT INTO Teacher(teacherName, email) 
+VALUES
+('Antonia', 'antonia@cifpfbmoll.eu'),
+('Josep', 'josep@cifpfbmoll.eu'),
+('Teresa', 'tresa@cifpfbmoll.eu'),
+('Maximo', 'max@cifpfbmoll.eu');
+
+INSERT INTO Company(companyName, email)
+VALUES
+('Barcelo', 'rrhh@barcelo.es'),
+('AirEuropa', 'rrhh@aireuropa.com'),
+('Melia', 'rh@melia.es'),
+('Hotelbeds', 'rh@hotelbeds.eu');
