f0092ac0e5f834f2d93b96dc5c4db54e8f4160b9
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-10-24 17:55:34 +0200

    change FK

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,40 +1,36 @@
 CREATE DATABASE IF NOT EXISTS borjamoll;
 USE borjamoll;
-
 DROP TABLE IF EXISTS Student;
 DROP TABLE IF EXISTS Employee;
 DROP TABLE IF EXISTS Company;
 DROP TABLE IF EXISTS Teacher;
 
 CREATE TABLE Teacher (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT primary key auto_increment,
     teacherName VARCHAR(250),
     email VARCHAR(250)
 );
 
 CREATE TABLE Company (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT primary key auto_increment,
     companyName VARCHAR(250),
     email VARCHAR(250)
 );
 
-CREATE TABLE Employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Employee(
+    id INT primary key auto_increment,
     employeeName VARCHAR(250),
     email VARCHAR(250),
-    company_id INT,
-    FOREIGN KEY (company_id) REFERENCES Company(id)
+    company_id INT REFERENCES Company(id)
 );
 
-CREATE TABLE Student (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Student(
+    id INT primary key auto_increment,
     studentName VARCHAR(250),
     surname VARCHAR(250),
     email VARCHAR(250),
-    teacher_id INT,
-    employee_id INT,
-    phone_number VARCHAR(15),
-    FOREIGN KEY (teacher_id) REFERENCES Teacher(id),
-    FOREIGN KEY (employee_id) REFERENCES Employee(id)
+    teacher_id INT REFERENCES Teacher(id),
+    employee_id INT REFERENCES Employee(id),
+    phone_number VARCHAR(15)
 );
 
