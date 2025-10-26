591b2fb783628d391862b41a97effc8e729f77ff
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-10-26 20:03:47 +0100

    comentarios

--- queriestama.sql
+++ queriestama.sql
@@ -1,23 +1,23 @@
--- SELECT AS WHERE <
+-- SELECT AS WHERE < - Selecionamos columnas, As - le ponemos alias , Where - condicion
 SELECT studentName AS Nombre, surname AS Apellido FROM Student WHERE id < 4;
 
--- DISCTINCT JOIN 
+-- DISCTINCT JOIN - Nombres sin repetir de estudiantes
 SELECT DISTINCT t.teacherName, s.studentName
 FROM Student s
 JOIN Teacher t ON s.teacher_id = t.id;
 
--- AND OR NOT
+-- AND OR NOT - condiciones AND ambas or o una o la otra
 SELECT * FROM Student WHERE teacher_id = 1 AND employee_id = 1;
 SELECT * FROM Student WHERE teacher_id = 1 OR teacher_id = 2;
 SELECT * FROM Student WHERE NOT teacher_id = 3;
 
--- ORDER BY JOIN
+--ORDER BY - Agrupa filas que tienen el mismo valor en una o varias columnas
  SELECT studentName AS Nombre, totalHour 
  FROM Student s
  JOIN Hour h ON s.id=h.student_id
  ORDER BY totalHour ASC;
 
--- MIN MAX COUNT SUM AVG
+-- MIN-minimo MAX-maximo COUNT-cuenta filas SUM-suma valores AVG-calcula la media
 SELECT COUNT(totalHour) AS NumeroDeFilas, 
 MIN(totalHour) AS MenosHoras, 
 MAX(totalHour) AS MásHoras,
@@ -25,7 +25,7 @@ SUM(totalHour) AS SumaTotal,
 AVG(totalHour) AS MediaTotal
 FROM Hour;
 
--- LIKE
+-- LIKE - Para buscar coincidencias segun el nombre del profesor que contenga M sin ser la ultima ni la primera y en email, si tiene una I con un @ y acaba en .eu indemendientemente de lo que tenga delante y entremedias.
 SELECT * FROM Teacher WHERE teacherName LIKE '%m%';
 SELECT * FROM Student WHERE email LIKE '%i@%.eu';
 
@@ -89,3 +89,9 @@ DROP COLUMN dateOfBirth,
 DROP COLUMN city, 
 DROP COLUMN nation;
 
+DELETE FROM Student
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Student 
+    GROUP BY studentName
+);
