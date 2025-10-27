4672ad07ca48b9879dfe998e7830ccc545188bfb
Tamara Fernandez Viturro <150025492+TamaraFdez@users.noreply.github.com>
2025-10-27 23:02:57 +0100

    Add new entries to queries.sql for various tables

--- queries.sql
+++ queries.sql
@@ -8,7 +8,10 @@ VALUES
 ('Antonia', 'antonia@cifpfbmoll.eu'),
 ('Josep', 'josep@cifpfbmoll.eu'),
 ('Teresa', 'tresa@cifpfbmoll.eu'),
-('Maximo', 'max@cifpfbmoll.eu');
+('Maximo', 'max@cifpfbmoll.eu'),
+('Mery Keys', 'mk@borjamoll.cat'),
+('Susana Storm', 'ss@borjamoll.cat'),
+('Ximo Bay', 'xb@borjamoll.cat');
 
 -- 🔹 COMPANIES
 INSERT INTO Company(companyName, email) 
@@ -18,7 +21,11 @@ VALUES
 ('Barcelo', 'rrhh@barcelo.es'),
 ('AirEuropa', 'rrhh@aireuropa.com'),
 ('Melia', 'rh@melia.es'),
-('Hotelbeds', 'rh@hotelbeds.eu');
+('Hotelbeds', 'rh@hotelbeds.eu'),
+('IBM', 'ibm@ibm.com'),
+('Apple World', 'applefake@tongo.com'),
+('Reciclajes PCS', 'reciclajespcs@pcs.com'),
+('WEB Consulting', 'webconsulting@consulting.com');
 
 -- 🔹 EMPLOYEES
 INSERT INTO Employee (employeeName, email, company_id) 
@@ -29,7 +36,11 @@ VALUES
 ('Aina', 'aina@barcelo.com', 1),
 ('Jaume', 'jaume@aireuropa.com', 2),
 ('Cristina', 'cristina@malia.es', 3),
-('Jesus', 'jesus@hotelbets.com', 4);
+('Jesus', 'jesus@hotelbets.com', 4),
+('Perpetual Khamid', 'pk@ibm.com', 1),
+('Socorro Plasencia', 'sp@tongo.com', 2),
+('Juanito Pérez', 'jp@pcs.com', 3),
+('Delegado de Legado', 'ddl@consulting.com',4);
 
 -- 🔹 STUDENTS
 INSERT INTO Student (studentName, surname, email, phoneNumber, teacher_id, employee_id) 
@@ -40,7 +51,11 @@ VALUES
 ('Jordi', 'Mas', 'jordi@cifpfbmoll.eu', '688999111', 2, 4),
 ('Paula', 'Garcia', 'paula@cifpfbmoll.eu', '677112233', 6, 2),
 ('Maria', 'German', 'MGerman@cipfbmoll.eu',"667896745",1,1),
-('Joan', 'Caminos','JCaminos@cifpfbmoll.eu',"665434748",2,2);
+('Joan', 'Caminos','JCaminos@cifpfbmoll.eu',"665434748",2,2),
+('Bad', 'Courier', 'bd@borjamoll.cat', '666554433', 1, 1),
+('Sonia', 'Irascuni', 'si@borjamoll.cat', '666777888', 1, 1),
+('Pep', 'Forani', 'pf@borjamoll.cat', '666778899', 2, 2),
+('Maria', 'Corrent', 'mc@borjamoll.cat', '666555444', 3, 3);
 
 -- 🔹 HOURS
 INSERT INTO Hour (totalHour, student_id) VALUES
@@ -50,5 +65,9 @@ INSERT INTO Hour (totalHour, student_id) VALUES
 (100, 4),
 (90, 5),
 (120, 6),
-(30, 7);
+(30, 7),
+(5,1),
+(12,2),
+(11,3),
+(18,4);
 
